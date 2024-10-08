817fddb11fbbb5c77b846f7974e1011e05d05338
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-08 19:42:03 +0200

    feat: :necktie: Added Boat entity

---
+++ Rise.Domain/Boats/IBoat.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rise.Domain.Boats
+{
+    public interface IBoat
+    {
+        public string PersonalName
+        {
+            get;
+            set;
+        }
+
+        public DateTime? StartOutOfOrder
+        {
+            get;
+        }
+
+        public DateTime? EndOutOfOrder
+        {
+            get;
+        }
+
+        public int MaximumAdults
+        {
+            get;
+            set;
+        }
+
+        public int MaximumChildren
+        {
+            get;
+            set;
+        }
+
+        public int MaximumPets
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Defines the period for how long the boat is out of order.
+        /// </summary>
+        public void DefineOutOfOrderPeriod(DateTime? startOutOfOrder, DateTime? endOutOfOrder = null);
+
+        /// <summary>
+        /// Removes out of orders Period
+        /// </summary>
+        public void RemoveOutOfOrderPeriod();
+
+        /// <summary>
+        /// Checks given date is in the out of order range
+        /// </summary>
+        public bool IsInOutOfOrderPeriod(DateTime date);
+    }
+}
