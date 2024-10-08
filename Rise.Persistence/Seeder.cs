7aacd7af556edb1c35180472491c1d12a54f89fd
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-08 22:03:44 +0200

    feat: :seedling: Added seeding for batteries

--- Rise.Persistence/Seeder.cs
+++ Rise.Persistence/Seeder.cs
@@ -22,6 +22,8 @@ public class Seeder
             SeedUsers();
         if (!dbContext.Boats.Any())
             SeedBoats();
+        if (!dbContext.Batteries.Any())
+            SeedBatteries();
     }
 
     private void SeedProducts()
@@ -58,6 +60,23 @@ public class Seeder
         boats[1].DefineOutOfOrderPeriod(DateTime.UtcNow);
         boats[2].DefineOutOfOrderPeriod(DateTime.UtcNow, DateTime.UtcNow.AddDays(2));
         dbContext.Boats.AddRange(boats);
+        dbContext.SaveChanges();
+    }
+
+    private void SeedBatteries()
+    {
+        var batteries = new List<Battery> {
+            new() {BoatId = 1, Type = "A", CurrentLoad = 75.24, MaximumCapacity = 100.0, OutOfOrder = false},
+            new() {BoatId = 1, Type = "A", CurrentLoad = 69.42, MaximumCapacity = 70.0, OutOfOrder = false},
+            new() {BoatId = 1, Type = "A", CurrentLoad = 24.56, MaximumCapacity = 100.0, OutOfOrder = true},
+            new() {BoatId = 2, Type = "A", CurrentLoad = 44.63, MaximumCapacity = 100.0, OutOfOrder = false},
+            new() {BoatId = 2, Type = "A", CurrentLoad = 4.91, MaximumCapacity = 100.0, OutOfOrder = false},
+            new() {BoatId = 2, Type = "B", CurrentLoad = 12.81, MaximumCapacity = 100.0, OutOfOrder = false},
+            new() {BoatId = 3, Type = "A", CurrentLoad = 58.41, MaximumCapacity = 86.0, OutOfOrder = false},
+            new() {BoatId = 3, Type = "C", CurrentLoad = 32.16, MaximumCapacity = 71.0, OutOfOrder = false},
+            new() {BoatId = 3, Type = "A", CurrentLoad = 33.21, MaximumCapacity = 98.0, OutOfOrder = false},
+        };
+        dbContext.Batteries.AddRange(batteries);
         dbContext.SaveChanges();
     }
 }
