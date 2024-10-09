0addf7e0ef5aa5b93e6221384afc363661f9540f
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-09 19:18:38 +0200

    feat: :card_file_box: Added Reservation to the database

---
+++ Rise.Persistence/Migrations/20241009161451_reservationsIntroduction.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Rise.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class reservationsIntroduction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reservation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    BoatId = table.Column<int>(type: "integer", nullable: false),
+                    BatteryId = table.Column<int>(type: "integer", nullable: false),
+                    TimeSlotId = table.Column<int>(type: "integer", nullable: false),
+                    AmountAdults = table.Column<int>(type: "integer", nullable: false),
+                    AmountChildren = table.Column<int>(type: "integer", nullable: false),
+                    AmountPets = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "NOW()"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "NOW()"),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservation_Battery_BatteryId",
+                        column: x => x.BatteryId,
+                        principalTable: "Battery",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservation_Boat_BoatId",
+                        column: x => x.BoatId,
+                        principalTable: "Boat",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservation_TimeSlots_TimeSlotId",
+                        column: x => x.TimeSlotId,
+                        principalTable: "TimeSlots",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReservationUser",
+                columns: table => new
+                {
+                    ReservationsId = table.Column<int>(type: "integer", nullable: false),
+                    UsersId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReservationUser", x => new { x.ReservationsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_ReservationUser_Reservation_ReservationsId",
+                        column: x => x.ReservationsId,
+                        principalTable: "Reservation",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ReservationUser_User_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_BatteryId",
+                table: "Reservation",
+                column: "BatteryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_BoatId",
+                table: "Reservation",
+                column: "BoatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_TimeSlotId",
+                table: "Reservation",
+                column: "TimeSlotId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReservationUser_UsersId",
+                table: "ReservationUser",
+                column: "UsersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReservationUser");
+
+            migrationBuilder.DropTable(
+                name: "Reservation");
+        }
+    }
+}
---
+++ Rise.Persistence/Timeslots/ReservationConfiguration.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Rise.Domain.Boats;
+using Rise.Domain.Timeslots;
+using Rise.Domain.Users;
+
+namespace Rise.Persistence.Timeslots
+{/// <summary>
+ /// Specific configuration for <see cref="Reservation"/>.
+ /// </summary>
+    internal class ReservationConfiguration : EntityConfiguration<Reservation>
+    {
+        public override void Configure(EntityTypeBuilder<Reservation> builder)
+        {
+            base.Configure(builder);
+            builder.Property(e => e.AmountAdults);
+            builder.Property(e => e.AmountChildren);
+            builder.Property(e => e.AmountPets);
+
+            builder
+                .HasOne(e => (Boat)e.Boat)
+                .WithMany(e => (ICollection<Reservation>)e.Reservations)
+                .HasForeignKey(e => e.BoatId)
+                .IsRequired(true);
+
+            builder
+                .HasOne(e => (Battery)e.Battery)
+                .WithMany(e => (ICollection<Reservation>)e.Reservations)
+                .HasForeignKey(e => e.BatteryId)
+                .IsRequired(true);
+
+            builder
+                .HasOne(e => (TimeSlot)e.TimeSlot)
+                .WithMany(e => (ICollection<Reservation>)e.Reservations)
+                .HasForeignKey(e => e.TimeSlotId)
+                .IsRequired(true);
+
+            builder
+                .HasMany(e => (ICollection<User>)e.Users)
+                .WithMany(e => (ICollection<Reservation>)e.Reservations);
+        }
+    }
+}
