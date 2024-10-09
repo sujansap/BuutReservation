68cf7ee83b60e23295e2bb2d81285ef7dd3e1f9f
Sujan Sapkota <sujan.sapkota@student.hogent.be>
2024-10-09 19:07:15 +0200

    RI2425G02-109 validation + test for TimeSlot and CruisePeriod

--- Rise.Domain/Timeslots/TimeSlot.cs
+++ Rise.Domain/Timeslots/TimeSlot.cs
@@ -1,12 +1,47 @@
+using Ardalis.GuardClauses;
+
 namespace Rise.Domain.Timeslots;
 
 public class TimeSlot : Entity, ITimeSlot
 {
-    // TODO make guard clauses for properties of TimeSlot
-    // TODO make tests for constructors TimeSlot
-    public DateOnly Date { get; set; }
-    public TimeSpan Start { get; set; }
-    public TimeSpan End { get; set; }
+    private TimeSpan _start;
+    private TimeSpan _end;
+    private DateOnly _date;
+
+    public DateOnly Date
+    {
+        get => _date;
+        set
+        {
+            Guard.Against.OutOfRange(value, nameof(Date), DateOnly.FromDateTime(DateTime.Today), DateOnly.MaxValue, "Date must be today or in the future.");
+            _date = value;
+        }
+    }
+
+    public TimeSpan Start
+    {
+        get => _start;
+        set
+        {
+            Guard.Against.OutOfRange(value, nameof(Start), TimeSpan.Zero, TimeSpan.FromHours(24), "Start time must be within a valid range.");
+
+            _start = value;
+        }
+    }
+
+    public TimeSpan End
+    {
+        get => _end;
+        set
+        {
+            Guard.Against.OutOfRange(value, nameof(End), TimeSpan.Zero, TimeSpan.FromHours(24), "End time must be within a valid range.");
+            if (_start != default && value <= _start)
+            {
+                throw new ArgumentOutOfRangeException(nameof(End), "End time must be after Start time.");
+            }
+            _end = value;
+        }
+    }
 
     public int CruisePeriodId { get; set; }
     public ICruisePeriod CruisePeriod { get; set; } = default!;
--- Rise.Persistence/Seeders/CruisePeriodSeeder.cs
+++ Rise.Persistence/Seeders/CruisePeriodSeeder.cs
@@ -11,7 +11,6 @@ public class CruisePeriodSeeder
     {
         this.dbContext = dbContext;
     }
-
     public void Seed()
     {
         if (HasAlreadyBeenSeeded())
@@ -29,30 +28,32 @@ public class CruisePeriodSeeder
         dbContext.CruisePeriods.AddRange(cruisePeriods);
         dbContext.SaveChanges();
 
-      
         foreach (var cruisePeriod in cruisePeriods)
         {
             var timeSlots = new List<TimeSlot>
+        {
+            new TimeSlot
             {
-                new TimeSlot
-                {
-                    Start = new TimeSpan(10, 0, 0), // 10:00 AM
-                    End = new TimeSpan(13, 0, 0),   // 1:00 PM
-                    CruisePeriodId = cruisePeriod.Id
-                },
-                new TimeSlot
-                {
-                    Start = new TimeSpan(14, 0, 0), // 2:00 PM
-                    End = new TimeSpan(17, 0, 0),   // 5:00 PM
-                    CruisePeriodId = cruisePeriod.Id
-                },
-                new TimeSlot
-                {
-                    Start = new TimeSpan(18, 0, 0), // 6:00 PM
-                    End = new TimeSpan(21, 0, 0),   // 9:00 PM
-                    CruisePeriodId = cruisePeriod.Id
-                }
-            };
+                Date = DateOnly.FromDateTime(cruisePeriod.Start),
+                Start = new TimeSpan(10, 0, 0), // 10:00 AM
+                End = new TimeSpan(13, 0, 0),   // 1:00 PM
+                CruisePeriodId = cruisePeriod.Id
+            },
+            new TimeSlot
+            {
+                Date = DateOnly.FromDateTime(cruisePeriod.Start),
+                Start = new TimeSpan(14, 0, 0), // 2:00 PM
+                End = new TimeSpan(17, 0, 0),   // 5:00 PM
+                CruisePeriodId = cruisePeriod.Id
+            },
+            new TimeSlot
+            {
+                Date = DateOnly.FromDateTime(cruisePeriod.Start),
+                Start = new TimeSpan(18, 0, 0), // 6:00 PM
+                End = new TimeSpan(21, 0, 0),   // 9:00 PM
+                CruisePeriodId = cruisePeriod.Id
+            }
+        };
 
             dbContext.TimeSlots.AddRange(timeSlots);
         }
