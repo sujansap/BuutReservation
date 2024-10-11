6074d80713f56d2d2835dd82dee798a89d489026
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-11 09:51:39 +0000

    Added dtos for seeing available calendar days

---
+++ Rise.Shared/TimeSlots/TimeSlotDayInfoDto.cs
@@ -0,0 +1,15 @@
+namespace Rise.Shared.TimeSlots
+{
+    /// <summary>
+    /// The surface info on the reservation state of a day
+    /// </summary>
+    /// <param name="Date">The date</param>
+    /// <param name="IsFullyBooked">If the day is fully booked</param>
+    /// <param name="IsSlotAvailable">If the day has a slot available to be booked</param>
+    public record class TimeSlotDayInfoDto(DateOnly Date, bool IsFullyBooked, bool IsSlotAvailable)
+    {
+
+    }
+
+
+}
