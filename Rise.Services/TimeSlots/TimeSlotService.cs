d2660d7396bd9953beca6913d3343b369d6f699a
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-11 09:47:15 +0000

    Update TimeSlotService.cs

--- Rise.Services/TimeSlots/TimeSlotService.cs
+++ Rise.Services/TimeSlots/TimeSlotService.cs
@@ -8,19 +8,14 @@ using System.Threading.Tasks;
 
 namespace Rise.Services.TimeSlots
 {
-    public class TimeSlotService : ITimeSlotService
+    public class TimeSlotService(ApplicationDbContext dbContext) : ITimeSlotService
     {
-        private readonly ApplicationDbContext _dbContext;
-
-        public TimeSlotService(ApplicationDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+        private readonly ApplicationDbContext dbContext = dbContext;
 
         public async Task<List<TimeSlotDto>> GetTimeSlotsByDate(DateTime date)
         {
             // Find the CruisePeriod that contains the given date
-            var cruisePeriod = await _dbContext.CruisePeriods
+            var cruisePeriod = await dbContext.CruisePeriods
                 .FirstOrDefaultAsync(cp => cp.Start.Date <= date.Date && cp.End.Date >= date.Date);
 
             if (cruisePeriod == null)
@@ -29,7 +24,7 @@ namespace Rise.Services.TimeSlots
             }
 
             // Fetch the TimeSlots for that CruisePeriod
-            var timeSlots = await _dbContext.TimeSlots
+            var timeSlots = await dbContext.TimeSlots
                 .Where(ts => ts.CruisePeriodId == cruisePeriod.Id)
                 .Select(ts => new TimeSlotDto
                 {
@@ -41,6 +36,57 @@ namespace Rise.Services.TimeSlots
                 .ToListAsync();
 
             return timeSlots;
+        }
+
+        public Task<TimeSlotRangeInfoDto> GetAllTimeSlots(
+            int year,
+            int month,
+            bool includeCrossOverDays)
+        {
+            var (startDay, endDay) = GenerateDayRange(year, month, includeCrossOverDays);
+            var task = new Task<TimeSlotRangeInfoDto>(
+                () =>
+                {
+                    int totalDays = 1 + endDay.Subtract(startDay).Days;
+                    IEnumerable<TimeSlotDayInfoDto> days = Enumerable.Range(0, totalDays)
+                    .Select(offset =>
+                    {
+                        var date = DateOnly.FromDateTime(startDay.AddDays(offset).Date);
+                        return new TimeSlotDayInfoDto(date, true, true);
+                    });
+                    return new TimeSlotRangeInfoDto(DateOnly.FromDateTime(startDay), DateOnly.FromDateTime(endDay), totalDays, days);
+                }
+            );
+            task.Start();
+            return task;
+        }
+
+        // TODO documentation
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="year"></param>
+        /// <param name="month"></param>
+        /// <param name="includeCrossOverDays"></param>
+        /// <returns></returns>
+        private static (DateTime, DateTime) GenerateDayRange(int year, int month, bool includeCrossOverDays)
+        {
+            // ? use local for calendars where Sunday is start of the week
+            DateOnly firstDayMonth = new(year, month, 1);
+            DateOnly lastDayMonth = firstDayMonth.AddMonths(1).AddDays(-1);
+
+            int firstDayIndex = NormalDayIndexToMonday(firstDayMonth.DayOfWeek);
+            int lastDayIndex = NormalDayIndexToMonday(lastDayMonth.DayOfWeek);
+
+            DateOnly startDay = includeCrossOverDays ? firstDayMonth.AddDays(1 - firstDayIndex) : firstDayMonth;
+            DateOnly endDay = includeCrossOverDays ? lastDayMonth.AddDays(7 - lastDayIndex) : lastDayMonth;
+
+            return (startDay.ToDateTime(new TimeOnly(0)), endDay.ToDateTime(new TimeOnly(0)));
+        }
+
+        private static int NormalDayIndexToMonday(DayOfWeek dayOfWeek)
+        {
+            return dayOfWeek == DayOfWeek.Sunday ? 7 : (int)dayOfWeek;
         }
     }
 }
