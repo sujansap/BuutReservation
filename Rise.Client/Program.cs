bb9b017efdfcaa9fe7a0fa8ccfdedec53d0062e1
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-10-11 12:27:48 +0200

    Added tests, but NavMenuShould.cs still contains errors

--- Rise.Client/Program.cs
+++ Rise.Client/Program.cs
@@ -13,6 +13,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
+builder.Services.AddMudPopoverService();
 
 
 // Register TimeSlotService
