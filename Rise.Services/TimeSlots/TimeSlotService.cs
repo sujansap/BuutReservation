2b4214e4bdca8ac126df195f7510239cac5c5a44
Sujan Sapkota <sujan.sapkota@student.hogent.be>
2024-10-14 17:22:44 +0200

    changed so that only available timeslots are returned by the api call

--- Rise.Services/TimeSlots/TimeSlotService.cs
+++ Rise.Services/TimeSlots/TimeSlotService.cs
@@ -16,21 +16,43 @@ namespace Rise.Services.TimeSlots
         {
             _dbContext = dbContext;
         }
-
         public async Task<IEnumerable<TimeSlotDto>> GetTimeSlotsByDate(int year, int month, int day)
         {
+            //Right now we don't keep the information of whether a boat is available or not
+            var amountOfAvailableBoats = await _dbContext.Boats.CountAsync();
+
             var date = new DateOnly(year, month, day);
 
-            var timeSlots = await _dbContext.TimeSlots.Where(ts => ts.Date == date).
-            Select(ts => new TimeSlotDto
+            var timeSlotReservationCounts = await _dbContext.TimeSlots
+            .Where(ts => ts.Date == date)
+            .Include(ts => ts.Reservations) // Eager load reservations
+            .GroupBy(ts => ts) // Group by each TimeSlot
+            .Select(g => new
             {
-                Id = ts.Id,
-                Start = ts.Start,
-                End = ts.End
-            }).ToListAsync();
+                TimeSlot = g.Key, // The timeslot
+                ReservationCount = g.SelectMany(ts => ts.Reservations).Count() // Count of reservations
+            })
+            .ToListAsync();
 
-            return timeSlots;
+            var availableTimeSlots = timeSlotReservationCounts
+            .Where(item => item.ReservationCount < amountOfAvailableBoats)
+            .Select(item => new TimeSlotDto
+            {
+                Id = item.TimeSlot.Id,
+                Start = item.TimeSlot.Start,
+                End = item.TimeSlot.End,
+                CruisePeriodId = item.TimeSlot.CruisePeriodId
+            })
+            .ToList();
+
+            //later this needs to change to check weather a timeslot has a reservation that is made by the
+            //curent logged in user 
+            //what we need to do is check if there is a reservation with userid and timeslotid from availableTimeSlots
+            return availableTimeSlots;
 
         }
+
+
+
     }
 }
