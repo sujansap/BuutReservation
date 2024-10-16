0a35ee2d66da18d06c743d41e3ae4a67d10af092
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-16 14:08:35 +0200

    build: :recycle: Included documentation of controllers into swagger generation

--- Rise.Server/Program.cs
+++ Rise.Server/Program.cs
@@ -12,7 +12,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
