5e1a4089fb3e24bf0275801250a23e04683900bc
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-16 22:11:36 +0200

    test: :building_construction: Added integration tests
    Added integration tests for the back-end, added a example test for products and updated readme to more clear about the database and testing.

---
+++ Rise.Server.Tests/Fixtures/ApiWebApplicationFactory.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Rise.Persistence;
+using Rise.Persistence.Triggers;
+
+namespace Rise.Server.Tests.Fixtures
+{
+    public class ApiWebApplicationFactory : WebApplicationFactory<Program>
+    {
+
+        public IConfiguration Configuration { get; private set; } = default!;
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(config =>
+            {
+                Configuration = new ConfigurationBuilder()
+                  .AddUserSecrets("d8435739-e257-4e40-b03f-9b9a66bbc18c")
+                  .Build();
+
+                config.AddConfiguration(Configuration);
+            });
+
+            // For stubbing services
+            builder.ConfigureServices(services =>
+            {
+                // Check if service exists
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions));
+
+                if (descriptor == null)
+                {
+                    // Connection
+                    services.AddDbContext<ApplicationDbContext>(options =>
+                    {
+                        options.UseNpgsql(Configuration.GetConnectionString("PostgreSQL"));
+                        options.UseTriggers(options => options.AddTrigger<EntityBeforeSaveTrigger>());
+                    });
+
+                    // Create and seed database
+                    services.AddSingleton<TestDatabaseInitializer>();
+                }
+            }
+            );
+        }
+    }
+
+    /// <summary>
+    /// Singleton service to initialize the database once
+    /// </summary>
+    internal class TestDatabaseInitializer
+    {
+        private static readonly object _lock = new();
+        private static bool _databaseInitialized = false;
+
+        public TestDatabaseInitializer(IServiceProvider serviceProvider)
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var scope = serviceProvider.CreateScope())
+                    {
+                        var scopedServices = scope.ServiceProvider;
+                        var db = scopedServices.GetRequiredService<ApplicationDbContext>();
+
+                        db.Database.EnsureDeleted();
+                        db.Database.EnsureCreated();
+
+                        // Ensure seeding is consistent 
+                        new Seeder(db).Seed();
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+        }
+    }
+}
---
+++ Rise.Server.Tests/Utils/ExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http.Json;
+
+namespace Rise.Server.Tests.Utils
+{
+    public static class ExtensionMethods
+    {
+        public static Task<T?> GetAndDeserialize<T>(this HttpClient client, string requestUri)
+        {
+            return client.GetFromJsonAsync<T>(requestUri);
+        }
+    }
+}
