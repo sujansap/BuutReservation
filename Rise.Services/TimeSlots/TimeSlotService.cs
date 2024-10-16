31c599a90b8b3efc7348b16601001313dea808cc
Sujan Sapkota <sujan.sapkota@student.hogent.be>
2024-10-16 15:49:04 +0200

    changed timeslot query to include timeslots without reservations

--- Rise.Services/TimeSlots/TimeSlotService.cs
+++ Rise.Services/TimeSlots/TimeSlotService.cs
@@ -18,23 +18,26 @@ namespace Rise.Services.TimeSlots
         }
         public async Task<IEnumerable<TimeSlotDto>> GetTimeSlotsByDate(int year, int month, int day)
         {
-
             //Right now we don't keep the information of whether a boat is available or not
             var amountOfAvailableBoats = await _dbContext.Boats.CountAsync();
 
             var date = new DateOnly(year, month, day);
 
+            //we get all the timeslots for a given date and the amount of reservations for that timeslot
             var timeSlotReservationCounts = await _dbContext.TimeSlots
             .Where(ts => ts.Date == date)
-            .Include(ts => ts.Reservations) // Eager load reservations
-            .GroupBy(ts => ts) // Group by each TimeSlot
-            .Select(g => new
+            .Select(ts => new
             {
-                TimeSlot = g.Key, // The timeslot
-                ReservationCount = g.SelectMany(ts => ts.Reservations).Count() // Count of reservations
+                TimeSlot = ts, // The timeslot itself
+                ReservationCount = ts.Reservations.Count() // how many times this timeslot is reserved
             })
             .ToListAsync();
 
+            //for debugging purposes
+            timeSlotReservationCounts.ForEach(item => Console.WriteLine(item.ReservationCount));
+
+            //if the timeslot is available we add it to the availableTimeSlots list
+            //we check whether the amount of reservations is less than the amount of boats for a given timeslot
             var availableTimeSlots = timeSlotReservationCounts
             .Where(item => item.ReservationCount < amountOfAvailableBoats)
             .Select(item => new TimeSlotDto
@@ -45,13 +48,12 @@ namespace Rise.Services.TimeSlots
             })
             .ToList();
 
-            //later this needs to change to check weather a timeslot has a reservation that is made by the
+            //later this needs to change to check whether a timeslot has a reservation that is made by the
             //curent logged in user 
             //what we need to do is check if there is a reservation with userid and timeslotid from availableTimeSlots
             return availableTimeSlots;
 
         }
-
 
 
     }
