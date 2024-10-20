85b483f7ff968fd3c3b5ff50b167d6fd5b301571
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-20 23:28:37 +0200

    refactor: :seedling: Added more and changed records for seeding surrounding: Cruise Period, Time Slot and Reservations

--- Rise.Persistence/Seeders/ProductSeeder.cs
+++ Rise.Persistence/Seeders/ProductSeeder.cs
@@ -6,17 +6,12 @@ namespace Rise.Persistence.Seeders
     internal class ProductSeeder(ApplicationDbContext dbContext) : GeneralSeeder<Product>(dbContext)
     {
 
-        private static readonly IList<Product> products = Enumerable.Range(1, 20)
+        internal static readonly IList<Product> products = Enumerable.Range(1, 20)
                                  .Select(i => new Product { Name = $"Product {i}" })
                                  .ToList();
 
-        internal override DbSet<Product> DbSet => dbContext.Products;
+        protected override DbSet<Product> DbSet => _dbContext.Products;
 
-        internal override ICollection<Product> Items { get => products; }
-
-        internal override bool HasAlreadyBeenSeeded()
-        {
-            return dbContext.Products.Any();
-        }
+        protected override ICollection<Product> Items { get => products; }
     }
 }
