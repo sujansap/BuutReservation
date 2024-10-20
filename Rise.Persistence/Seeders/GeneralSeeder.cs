6364aecd58a6964c48f8d84965dd654d0ec878a2
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-20 23:28:37 +0200

    refactor: :seedling: Added more and changed records for seeding surrounding: Cruise Period, Time Slot and Reservations

--- Rise.Persistence/Seeders/GeneralSeeder.cs
+++ Rise.Persistence/Seeders/GeneralSeeder.cs
@@ -2,12 +2,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Rise.Persistence.Seeders
 {
+    /// <summary>
+    /// General template for seeding an Entity
+    /// </summary>
+    /// <typeparam name="T">the entity in the database</typeparam>
+    /// <param name="dbContext">Database context to perform seeding with/on</param>
     internal abstract class GeneralSeeder<T>(ApplicationDbContext dbContext) where T : class
     {
 
-        internal readonly ApplicationDbContext dbContext = dbContext;
-        internal abstract ICollection<T> Items { get; }
-        internal abstract DbSet<T> DbSet { get; }
+        protected readonly ApplicationDbContext _dbContext = dbContext;
+
+        protected abstract ICollection<T> Items { get; }
+        protected abstract DbSet<T> DbSet { get; }
 
         public void Seed()
         {
@@ -17,13 +23,18 @@ namespace Rise.Persistence.Seeders
             }
         }
 
-        internal abstract bool HasAlreadyBeenSeeded();
-
-        internal void PersistItemsToDatabase()
+        /// <returns>If the database already has been seeded with record for the entity</returns>
+        protected virtual bool HasAlreadyBeenSeeded()
         {
-            DbSet.AddRange(Items);
-            dbContext.SaveChanges();
+            return DbSet.Any();
         }
 
+        /// <summary>Adds the seeding records to the database</summary>
+
+        protected void PersistItemsToDatabase()
+        {
+            DbSet.AddRange(Items);
+            _dbContext.SaveChanges();
+        }
     }
 }
