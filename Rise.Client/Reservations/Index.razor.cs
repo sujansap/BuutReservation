dc4dd2ebdc9c74edf9eb7b44bb5851750352adb3
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-21 20:51:36 +0200

    fix: :bug: Fixed calendar component not showing the available days and booked days correctly

--- Rise.Client/Reservations/Index.razor.cs
+++ Rise.Client/Reservations/Index.razor.cs
@@ -32,10 +32,6 @@ namespace Rise.Client.Reservations
         private ITimeSlotService TimeSlotService { get; set; } = default!;
 
         /// <summary>
-        /// All available days on the calendar
-        /// </summary>
-        private List<ColoredCalendarItem> AvailableDays = [];
-        /// <summary>
         /// All unavailable days on the calendar
         /// </summary>
         private List<DateTime> GreyedOutDates = [];
@@ -113,12 +109,12 @@ namespace Rise.Client.Reservations
             );
 
 
-                AvailableDays = response.Days
-                .Where(day => day.IsSlotAvailable && !day.IsFullyBooked)
-                .Select(ConvertToCalendarItems)
-                .ToList();
+                // AvailableDays = response.Days
+                // .Where(day => day.IsSlotAvailable && !day.IsFullyBooked)
+                // .Select(ConvertToCalendarItems)
+                // .ToList();
 
-                GreyedOutDates = response.Days.Where(day => day.IsFullyBooked).Select(day => day.Date.ToDateTime(TimeOnly.MinValue)).ToList();
+                GreyedOutDates = response.Days.Where(day => day.IsFullyBooked || !day.IsSlotAvailable).Select(day => day.Date.ToDateTime(TimeOnly.MinValue)).ToList();
             }
 
             catch
