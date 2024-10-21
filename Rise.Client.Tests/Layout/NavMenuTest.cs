811a0339c48b461e132a4c4cf851d1aa04cc7c77
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-21 20:03:40 +0200

    refactor: :recycle: Replaced home and about to actual placeholder

--- Rise.Client.Tests/Layout/NavMenuTest.cs
+++ Rise.Client.Tests/Layout/NavMenuTest.cs
@@ -14,7 +14,8 @@ namespace Rise.Client.Layout
         private const int DefaultHeight = 1920;
 
         [TestMethod]
-        [DataRow("nav-brand-logo", "/", "/reservations")]
+        [DataRow("nav-brand-logo", "/home", "/reservations")]
+        [DataRow("nav-desktop-home", "/home", "/reservations")]
         [DataRow("nav-desktop-about", "/about", "")]
         [DataRow("nav-desktop-reservations", "/reservations", "")]
         [DataRow("nav-desktop-book", "/book", "")]
@@ -32,15 +33,18 @@ namespace Rise.Client.Layout
         }
 
         [TestMethod]
-        [DataRow("nav-mobile-about", "about")]
-        [DataRow("nav-mobile-profile", "profile")]
-        [DataRow("nav-mobile-reservations", "reservations")]
-        [DataRow("nav-mobile-book", "book")]
-        [DataRow("nav-mobile-notifications", "notifications")]
-        public async Task Mobile_NavNotifications(string testId, string resultSuffix)
+        [DataRow("nav-mobile-home", "home", "/reservations")]
+        [DataRow("nav-mobile-about", "about", "")]
+        [DataRow("nav-mobile-profile", "profile", "")]
+        [DataRow("nav-mobile-reservations", "reservations", "")]
+        [DataRow("nav-mobile-book", "book", "")]
+        [DataRow("nav-mobile-notifications", "notifications", "")]
+        [DataRow("nav-mobile-profile", "profile", "")]
+        [DataRow("nav-mobile-notifications", "notifications", "")]
+        public async Task Mobile_NavNotifications(string testId, string resultSuffix, string startSuffix)
         {
             await Page.SetViewportSizeAsync(959, 1920);
-            await Page.GotoAsync("https://localhost:5001");
+            await Page.GotoAsync("https://localhost:5001" + startSuffix);
             string beginUri = Page.Url;
             await Page.GetByTestId("nav-drawer-open-button").ClickAsync();
             await Page.GetByTestId(testId).ClickAsync();
