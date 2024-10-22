f4948dcb57f32565ed2bf80217a26f1c9754a53d
Arthur Van Ginderachter <arthur@vanginderachter.be>
2024-10-23 01:10:59 +0200

    workflow test

--- dockerrunner.sh
+++ dockerrunner.sh
@@ -13,3 +13,4 @@ exec dotnet /app/Rise.Server.dll --urls "http://0.0.0.0:${http_port};https://0.0
 #docker run -it -e DB_USERNAME=Hogent -e DB_PASSWORD=Hogent2425 -e DB_IP=172.17.0.3 -e DB_PORT=5432 -e DB_NAME="Hogent.Rise" -e http_port=5000 -e https_port=5001  -p 5000:5000 -p 5001:5001 mydotnetapp /bin/bash
 
 
+  
