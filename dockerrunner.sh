5dafca6a384167f89fb4c58d58408c1e3279ad90
Arthur Van Ginderachter <arthur@vanginderachter.be>
2024-10-22 17:09:35 +0200

    DockerFile auotmation + inside bash script for container

---
+++ dockerrunner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status
+set -e
+# Treat unset variables as an error when substituting
+set -u
+# Print each command before executing it (for debugging purposes)
+set -x
+export ConnectionStrings__PostgreSQL="user ID=${DB_USERNAME};Password=${DB_PASSWORD};Host=${DB_IP};Port=${DB_PORT};Database=${DB_NAME};Connection Lifetime=0;"
+
+
+
+exec dotnet /app/Rise.Server.dll --urls "http://0.0.0.0:${http_port};https://0.0.0.0:${https_port}" --environment Production
+#docker run -it -e DB_USERNAME=Hogent -e DB_PASSWORD=Hogent2425 -e DB_IP=172.17.0.3 -e DB_PORT=5432 -e DB_NAME="Hogent.Rise" -e http_port=5000 -e https_port=5001  -p 5000:5000 -p 5001:5001 mydotnetapp /bin/bash
+
+
