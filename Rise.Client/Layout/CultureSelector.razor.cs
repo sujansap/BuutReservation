17c6a20c81dfd6441ccfc873ef47898097b3aa8d
Sujan Sapkota <sujan.sapkota@student.hogent.be>
2024-10-22 19:31:18 +0200

    refactor: changed the location of CultureSelector component

--- Rise.Client/Layout/CultureSelector.razor.cs
+++ Rise.Client/Layout/CultureSelector.razor.cs
@@ -3,38 +3,59 @@ using Microsoft.JSInterop;
 using MudBlazor;
 using System.Globalization;
 
-namespace Rise.Client.Components
+namespace Rise.Client.Layout
 {
     public partial class CultureSelector
     {
+
+        /// <summary>
+        /// The color of the menu, default is <see cref="Color.Secondary"/>.
+        /// </summary>
         [Parameter]
         public Color MenuColor { get; set; } = Color.Secondary;
 
+
+        /// <summary>
+        /// The cultures that are supported by the application.
+        /// </summary> 
         private CultureInfo[] supportedCultures = new[]
         {
         new CultureInfo("nl-BE"),
         new CultureInfo("en-US"),
         };
 
+        /// <summary>
+        /// The currently selected culture. 
+        /// </summary>
         private CultureInfo? selectedCulture;
 
+        /// <summary>
+        ///
+        /// </summary>
         protected override void OnInitialized()
         {
             selectedCulture = CultureInfo.CurrentCulture;
         }
 
+        /// <summary>
+        ///  Handles the selection of a culture.
+        /// </summary>
         private async Task OnCultureSelectedAsync(CultureInfo culture)
         {
             selectedCulture = culture;
             await ApplySelectedCultureAsync();
         }
 
+        /// <summary>
+        /// Applies the selected culture 
+        /// updates the localStorage and refreshes the page.
+        /// </summary>
         private async Task ApplySelectedCultureAsync()
         {
             if (CultureInfo.CurrentCulture != selectedCulture)
             {
                 await JS.InvokeVoidAsync("blazorCulture.set", selectedCulture!.Name);
-                Navigation.NavigateTo(Navigation.Uri, forceLoad: true);
+                Navigation.Refresh(true);
             }
         }
     }
