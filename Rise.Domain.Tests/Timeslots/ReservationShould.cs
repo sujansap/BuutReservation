c5d4c2a4c812155183b82771adac1f541ce0bc3d
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-24 10:41:25 +0200

    test: :white_check_mark: Removed test for checking creation of reservation with user list

--- Rise.Domain.Tests/Timeslots/ReservationShould.cs
+++ Rise.Domain.Tests/Timeslots/ReservationShould.cs
@@ -11,28 +11,6 @@ namespace Rise.Domain.Tests.Timeslots
 {
     public class ReservationShould
     {
-        // FIXME: DO WE NEED THIS TEST? A RESERVATION CAN'T EXIT WITHOUT THE USER WHO MADE IT WITH THE CURRENT IMPLEMENTATION
-        // [Fact]
-        // public void BeCreatedWithoutUser()
-        // {
-        //     // Mock boat
-        //     IBoat mockBoat = Substitute.For<IBoat>();
-
-        //     // Mock Time slot
-        //     ITimeSlot mockTimeSlot = Substitute.For<ITimeSlot>();
-
-        //     Reservation reservation = new()
-        //     {
-        //         Boat = mockBoat,
-        //         TimeSlot = mockTimeSlot,
-        //     };
-
-        //     // Act & Assert
-        //     reservation.Boat.ShouldBe(mockBoat);
-        //     reservation.TimeSlot.ShouldBe(mockTimeSlot);
-        //     reservation.User.ShouldBeEmpty();
-        // }
-
         [Fact]
         public void BeCreatedWithUser()
         {
