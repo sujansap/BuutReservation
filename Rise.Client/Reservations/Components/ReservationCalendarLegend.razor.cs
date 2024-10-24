2385b9c7e4cf12a0458a31d248f99c3517363ff6
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-24 18:00:42 +0200

    fix: :rotating_light: Made optional attributes actually optionals on Reservation Calendar

---
+++ Rise.Client/Reservations/Components/ReservationCalendarLegend.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Rise.Client.Reservations.Components;
+
+public partial class ReservationCalendarLegend
+{
+  [Parameter(CaptureUnmatchedValues = true)]
+  public Dictionary<string, object>? AdditionalAttributes { get; set; }
+}
