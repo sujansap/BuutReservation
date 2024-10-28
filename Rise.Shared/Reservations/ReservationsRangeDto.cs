b759582988ef4ac8bdc76bee89e21589cf50ea2d
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-10-28 13:27:52 +0100

    docs: :memo: Added documentation for the ReservationRangeDto

--- Rise.Shared/Reservations/ReservationsRangeDto.cs
+++ Rise.Shared/Reservations/ReservationsRangeDto.cs
@@ -1,5 +1,9 @@
 namespace Rise.Shared.Reservations
 {
+    /// <summary>
+    /// Represents a list DateOnly objects on wich there are reservations
+    /// </summary>
+    /// <param name="Reservations">List of DateOnly objects on wich there are reservations</param>
     public record ReservationsRangeDto(IEnumerable<DateOnly> Reservations)
     {
 
