c843c898669320c5b118eab887700df88067beb9
Xan <xan.pinson@student.hogent.be>
2024-10-28 18:21:22 +0100

    fix: :construction: Bug pagination (No reservations found)
    Fixing issues so app works but still issues, When trying to get the reservations none are found while database should be alright.

--- Rise.Shared/Pagination/ItemsPageDto.cs
+++ Rise.Shared/Pagination/ItemsPageDto.cs
@@ -1,11 +1,15 @@
-using System;
-
-namespace Rise.Shared.Pagination;
-
-public class ItemsPageDto<T>
+namespace Rise.Shared.Pagination
 {
-    public IEnumerable<T> Data { get; set; }
+    public class ItemsPageDto<T>
+    {
+        public IEnumerable<T> Data { get; set; }
     public int? NextId { get; set; }
     public int? PreviousId { get; set; }
     public bool IsFirstPage { get; set; }
+    public bool CanGoBack { get; set; } // Add this
+    public bool CanGoForward { get; set; } // Add this
+    public int? ForwardCursor { get; set; } // Add this
+    public int? BackwardCursor { get; set; } // Add this
+}
+
 }
