7c49e1e02745dc56890ab387e08e925f9b06a81a
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-29 18:39:44 +0100

    test: :bug: Fixed test that checks for calendar cell amount

--- Rise.Client.Tests/Reservations/ReservationPageTest.cs
+++ Rise.Client.Tests/Reservations/ReservationPageTest.cs
@@ -8,7 +8,6 @@ namespace Rise.Client.Reservations
     [TestFixture]
     public class ReservationPageTest : CustomPageTest
     {
-        private const string universalDateFormat = "yyyy-MM-dd";
 
         [Test]
         public async Task HasTabs()
@@ -30,14 +29,10 @@ namespace Rise.Client.Reservations
         {
             await Page.GotoAsync("/reservations");
 
-            // Wait for the page to load
-            await Page.WaitForLoadStateAsync(LoadState.NetworkIdle);
-
-            // Use a simple class selector to find all calendar day cells
-            var days = await Page.Locator(".mud-cal-month-cell").AllAsync();
+            ILocator daysLocator = Page.GetByTestId("calendar-cel");
 
             // Assert the correct number of days
-            Assert.That(days.Count, Is.EqualTo(35), "The calendar should have 35 day elements (5 weeks * 7 days)");
+            await Expect(daysLocator).ToHaveCountAsync(35);
         }
 
         [Test]
