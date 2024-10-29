bca06de41cd33e44e839c1866efc0f4c9dbaf849
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-29 16:11:03 +0100

    fix: :globe_with_meridians: Fixed own reservation page not using the correct resources

--- Rise.Client/Localization/Reservations/ReservationPageResources.cs
+++ Rise.Client/Localization/Reservations/ReservationPageResources.cs
@@ -2,19 +2,5 @@ namespace Rise.Client.Localization.Reservations;
 
 public class ReservationPageResources
 {
-    public const string NoReservationsFound = "Geen reservaties gevonden.";
-    public const string ReservationFor = "Reservatie voor";
-    public const string Boat = "Boot";
-    public const string Time = "Tijd";
-    public const string ViewDetails = "Zie details";
-    public const string Previous = "Vorige";
-    public const string Next = "Volgende";
-    public const string Reserve = "Reserveer";
-    public const string YourReservations = "Uw Reservaties";
-    public const string LoadingReservations = "Reserveringen laden...";
-    public const string ErrorLoadingReservations = "Er is iets mis gegaan bij het ophalen van de reserveringen.";
-    public const string Date = "Datum";
-    public const string FullyBooked = "Volzet";
-    public const string NotAvailable = "Niet beschikbaar";
-    public const string SelectDate = "Selecteer datum";
+
 }
