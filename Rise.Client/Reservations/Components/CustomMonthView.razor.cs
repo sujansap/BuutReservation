9e11dd3c606058d2c36f6e43c4ef4a8419e90790
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-10-29 19:49:47 +0100

    feat: :sparkles: Added cell type labeling on month view for calendar

--- Rise.Client/Reservations/Components/CustomMonthView.razor.cs
+++ Rise.Client/Reservations/Components/CustomMonthView.razor.cs
@@ -5,35 +5,43 @@ using Rise.Shared.TimeSlots;
 
 namespace Rise.Client.Reservations.Components
 {
-  public partial class CustomMonthView
-  {
-    [Parameter]
-    public Dictionary<DateOnly, TimeSlotDaySurfaceInfoDto> AvailableDays { get; set; } = [];
-    public DateTime Today { get; set; } = DateTime.Today;
-    protected override string DayClassname(CalendarCell calendarCell)
+    public partial class CustomMonthView
     {
-      return new CssBuilder(base.DayClassname(calendarCell))
-      .AddClass("greyed-out-text", IsGreyedOut(calendarCell))
-      .Build();
-    }
-    protected override string DayStyle(CalendarCell calendarCell, int index)
-    {
-      bool isToday = calendarCell.Date.Date.Equals(Today);
-      bool isGreyedOut = IsGreyedOut(calendarCell);
+        [Parameter]
+        public Dictionary<DateOnly, TimeSlotDaySurfaceInfoDto> AvailableDays { get; set; } = [];
+        public DateTime Today { get; set; } = DateTime.Today;
+        protected override string DayClassname(CalendarCell calendarCell)
+        {
+            return new CssBuilder(base.DayClassname(calendarCell))
+            .AddClass("greyed-out-text", IsGreyedOut(calendarCell))
+            .Build();
+        }
+        protected override string DayStyle(CalendarCell calendarCell, int index)
+        {
+            bool isToday = calendarCell.Date.Date.Equals(Today);
+            bool isGreyedOut = IsGreyedOut(calendarCell);
 
-      return new StyleBuilder()
-      .AddStyle(base.DayStyle(calendarCell, index))
-      .AddStyle("border", "var(--custom-today-border)", isToday)
-      .AddStyle("background-color", "var(--custom-grey-out-color)", isGreyedOut)
-      .AddStyle("color", "var(--mud-palette-text-disabled)", isGreyedOut)
-      .Build();
-    }
+            return new StyleBuilder()
+            .AddStyle(base.DayStyle(calendarCell, index))
+            .AddStyle("border", "var(--custom-today-border)", isToday)
+            .AddStyle("background-color", "var(--custom-grey-out-color)", isGreyedOut)
+            .AddStyle("color", "var(--mud-palette-text-disabled)", isGreyedOut)
+            .Build();
+        }
 
-    private bool IsGreyedOut(CalendarCell calendarCell)
-    {
-      return !AvailableDays.ContainsKey(DateOnly.FromDateTime(calendarCell.Date));
+        private bool IsGreyedOut(CalendarCell calendarCell)
+        {
+            return !AvailableDays.ContainsKey(DateOnly.FromDateTime(calendarCell.Date));
+        }
+
+        private string GetCellTypes(CalendarCell calendarCell)
+        {
+            if (IsGreyedOut(calendarCell))
+                return "fully-booked";
+
+            return "available";
+        }
     }
-  }
 }
 
 
