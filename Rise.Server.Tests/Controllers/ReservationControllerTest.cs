8759a853287301ec5f453f99c6087730dcd973b2
Pushwant Sagoo <pushwant.sagoo@student.hogent.be>
2024-10-30 21:00:20 +0100

    fix: :fire: fix integration tests not working and remove code in comments

--- Rise.Server.Tests/Controllers/ReservationControllerTest.cs
+++ Rise.Server.Tests/Controllers/ReservationControllerTest.cs
@@ -9,12 +9,12 @@ using Rise.Shared.Reservations;
 
 namespace Rise.Server.Tests.Controllers
 {
-    public class ReservationControllerTest(ApiWebApplicationFactory fixture) : IntegrationTest(fixture)
+    public class ReservationControllerTest(ApiWebApplicationFactory fixture) : IntegrationTest(fixture, "Reservation")
     {
         [Fact]
         public async Task GET_CurrentUser_UpcomingReservations_WithNoParameters_FirstPage_ExpectOk_5OrLessReservations()
         {
-            var response = await _client.GetAsync($"Reservation/me");
+            var response = await _client.GetAsync("me");
             response.StatusCode.ShouldBe(HttpStatusCode.OK);
 
             var reservationsPage = await response.Content.ReadFromJsonAsync<ItemsPageDto<ReservationDto>>();
@@ -30,7 +30,7 @@ namespace Rise.Server.Tests.Controllers
         [Fact]
         public async Task GET_CurrentUser_PastReservations_WithNoParameters_FirstPage_ExpectOk_5OrLessReservations()
         {
-            var response = await _client.GetAsync($"Reservation/me?getPast=true");
+            var response = await _client.GetAsync("me?getPast=true");
             response.StatusCode.ShouldBe(HttpStatusCode.OK);
 
             var reservationsPage = await response.Content.ReadFromJsonAsync<ItemsPageDto<ReservationDto>>();
@@ -49,7 +49,7 @@ namespace Rise.Server.Tests.Controllers
         [InlineData(15)]
         public async Task GET_CurrentUser_UpcomingReservations_WithVaryingPageSize_ExpectOk_PageSizeAmountOrLessReservations(int pageSize)
         {
-            var response = await _client.GetAsync($"Reservation/me?pageSize={pageSize}");
+            var response = await _client.GetAsync($"me?pageSize={pageSize}");
             response.StatusCode.ShouldBe(HttpStatusCode.OK);
 
             var reservationsPage = await response.Content.ReadFromJsonAsync<ItemsPageDto<ReservationDto>>();
@@ -63,7 +63,7 @@ namespace Rise.Server.Tests.Controllers
         [InlineData(0)]
         public async Task GET_CurrentUser_Reservations_WithInvalidPageSize_ExpectBadRequest(int pageSize)
         {
-            var response = await _client.GetAsync($"Reservation/me?pageSize={pageSize}");
+            var response = await _client.GetAsync($"me?pageSize={pageSize}");
             response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
         }
 
@@ -71,7 +71,7 @@ namespace Rise.Server.Tests.Controllers
         [InlineData("invalid")]
         public async Task GET_CurrentUser_Reservations_WithInvalidPageSizeType_ExpectBadRequest(string pageSize)
         {
-            var response = await _client.GetAsync($"Reservation/me?pageSize={pageSize}");
+            var response = await _client.GetAsync($"me?pageSize={pageSize}");
             response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
         }
 
@@ -80,7 +80,7 @@ namespace Rise.Server.Tests.Controllers
         [InlineData(-56)]
         public async Task GET_CurrentUser_Reservations_WithInvalidNumber_ExpectBadRequest(int cursor)
         {
-            var response = await _client.GetAsync($"Reservation/me?cursor={cursor}");
+            var response = await _client.GetAsync($"me?cursor={cursor}");
             response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
         }
 
@@ -88,14 +88,14 @@ namespace Rise.Server.Tests.Controllers
         [InlineData("invalid")]
         public async Task GET_CurrentUser_Reservations_WithInvalidType_ExpectBadRequest(string cursor)
         {
-            var response = await _client.GetAsync($"Reservation/me?cursor={cursor}");
+            var response = await _client.GetAsync($"me?cursor={cursor}");
             response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
         }
 
         [Fact]
-        public async Task GET_CurrentUser_Reservations_NextPage_WithValidCursor_InavlidIsNextPage_ExpectBadRequest()
+        public async Task GET_CurrentUser_Reservations_NextPage_WithValidCursor_IsNextPageNull_ExpectBadRequest()
         {
-            var response = await _client.GetAsync($"Reservation/me?cursor=17");
+            var response = await _client.GetAsync($"me?cursor=17");
             response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
         }
     }
--- Rise.Services/Reservations/ReservationService.cs
+++ Rise.Services/Reservations/ReservationService.cs
@@ -9,7 +9,6 @@ using Rise.Shared.Reservations;
 
 namespace Rise.Services.Reservations
 {
-
     public class ReservationService(ApplicationDbContext dbContext) : IReservationService
     {
         private readonly ApplicationDbContext _dbContext = dbContext;
@@ -43,84 +42,6 @@ namespace Rise.Services.Reservations
 
         public async Task<ItemsPageDto<ReservationDto>> GetUserReservations(int userId, int? cursor, bool? isNextPage, bool getPast = false, int pageSize = 5)
         {
-            //     IQueryable<IReservation> reservationsQuery = _dbContext.Reservations
-            //         .Where(r => (r.UserId == userId) && (getPast ?
-            //            r.TimeSlot.Date < DateOnly.FromDateTime(DateTime.Now) :
-            //            r.TimeSlot.Date >= DateOnly.FromDateTime(DateTime.Now)))
-            //         .OrderBy(r => r.TimeSlot.Date).ThenBy(r => r.Id);
-
-            //     int takeAmount = pageSize + 1;
-
-            //     if (cursor is not null)
-            //     {
-            //         if (isNextPage == true)
-            //         {
-            //             reservationsQuery = reservationsQuery.Where(r => r.Id > cursor);
-            //         }
-            //         else
-            //         {
-            //             reservationsQuery = reservationsQuery.Where(r => r.Id < cursor);
-            //             takeAmount = pageSize;
-            //         }
-            //     }
-
-            //     reservationsQuery = reservationsQuery.Take(takeAmount);
-
-            //     var reservations = await reservationsQuery
-            //         .Select(r => new ReservationDto
-            //         {
-            //             Id = r.Id,
-            //             Start = r.TimeSlot.Start,
-            //             End = r.TimeSlot.End,
-            //             Date = r.TimeSlot.Date,
-            //             BoatId = r.BoatId,
-            //             BoatPersonalName = r.Boat.PersonalName
-            //         })
-            //         .ToListAsync();
-
-            //     if (reservations.Count == 0)
-            //     {
-            //         return new ItemsPageDto<ReservationDto>
-            //         {
-            //             Data = [],
-            //             IsFirstPage = true,
-            //             NextId = null,
-            //             PreviousId = null
-            //         };
-            //     }
-
-            //     bool isFirstPage = !cursor.HasValue ||
-            //                          (cursor.HasValue && reservations.FirstOrDefault()?.Id ==
-            //                          _dbContext.Reservations
-            //                          .Where(r => (r.UserId == userId) && (getPast ?
-            //                             r.TimeSlot.Date < DateOnly.FromDateTime(DateTime.Now) :
-            //                             r.TimeSlot.Date >= DateOnly.FromDateTime(DateTime.Now)))
-            //                         .OrderBy(r => r.TimeSlot.Date).ThenBy(r => r.Id).FirstOrDefault()?.Id);
-
-            //     bool hasNextPage = reservations.Count > pageSize ||
-            //                         (cursor is not null && isNextPage == false);
-
-            //     if (reservations.Count > pageSize)
-            //     {
-            //         reservations.RemoveAt(reservations.Count - 1);
-            //     }
-
-            //     int? nextId = hasNextPage
-            //             ? reservations.LastOrDefault()?.Id
-            //             : null;
-
-            //     int? previousId = reservations.Count > 0 && !isFirstPage
-            //             ? reservations.FirstOrDefault()?.Id
-            //             : null;
-
-            //     return new ItemsPageDto<ReservationDto>
-            //     {
-            //         Data = reservations,
-            //         IsFirstPage = isFirstPage,
-            //         NextId = nextId,
-            //         PreviousId = previousId
-            //     };
-
             return await PaginationService.GetPaginatedResultsAsync<IReservation, ReservationDto>(
                 queryableDbSet: _dbContext.Reservations.AsQueryable(),
                 filterLambda: r => (r.UserId == userId) && (getPast ?
@@ -149,7 +70,4 @@ namespace Rise.Services.Reservations
             );
         }
     }
-
-
-
 }
