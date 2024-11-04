ff2ef4d519ca40713d95eeed619fb22ebda4be3f
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-11-04 16:40:53 +0100

    refactor: :recycle: Made GetUserReservations use web utilities to make query parameters

--- Rise.Client/Reservations/Components/ReservationService.cs
+++ Rise.Client/Reservations/Components/ReservationService.cs
@@ -1,7 +1,8 @@
 using System.Net.Http.Json;
 using Rise.Shared.Pagination;
 using Rise.Shared.Reservations;
-// FIXME
+using Microsoft.AspNetCore.WebUtilities;
+
 namespace Rise.Client.Services
 {
     public class ReservationService(HttpClient httpClient) : IReservationService
@@ -21,22 +22,19 @@ namespace Rise.Client.Services
 
         public async Task<ItemsPageDto<ReservationDto>> GetUserReservations(int userId, int? cursor, bool? isNextPage, bool getPast = false, int pageSize = 5)
         {
-            Console.WriteLine("HERE: GetUserReservations");
-            //cursor=1&isNextPage=false&getPast=false&pageSize=1' \
-            var query = new List<string>();
-            query.Add($"cursor={cursor}");
-            query.Add($"isNextPage={isNextPage}");
-            query.Add($"getPast={getPast}");
-            query.Add($"pageSize={pageSize}");
+            Dictionary<string, string?> queries = new()
+            {
+                ["cursor"] = cursor.ToString(),
+                ["isNextPage"] = isNextPage.ToString(),
+                ["getPast"] = getPast.ToString(),
+                ["pageSize"] = pageSize.ToString(),
+            };
 
-            var queryString = string.Join("&", query);
-            Console.WriteLine("Query: " + queryString);
+            string queryString = QueryHelpers.AddQueryString("me", queries);
 
-            var result = await _httpClient.GetFromJsonAsync<ItemsPageDto<ReservationDto>>($"me?{queryString}")
-                ?? new ItemsPageDto<ReservationDto> { Data = Enumerable.Empty<ReservationDto>() };
+            ItemsPageDto<ReservationDto> result = await _httpClient.GetFromJsonAsync<ItemsPageDto<ReservationDto>>(queryString)
+                ?? new ItemsPageDto<ReservationDto> { Data = [] };
 
-
-            Console.WriteLine("Result: " + result.Data.Count());
             return result;
 
         }
