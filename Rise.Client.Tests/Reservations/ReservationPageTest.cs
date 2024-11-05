c77218f5dfc2f17fd4327aaeca8e30fb5d2aaa48
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-11-05 18:46:00 +0100

    test: :white_check_mark: Removed traceview for ReservationPageTest

--- Rise.Client.Tests/Reservations/ReservationPageTest.cs
+++ Rise.Client.Tests/Reservations/ReservationPageTest.cs
@@ -8,32 +8,6 @@ namespace Rise.Client.Reservations
     [TestFixture]
     public class ReservationPageTest : CustomPageTest
     {
-
-        [SetUp]
-        public async Task Setup()
-        {
-            await Context.Tracing.StartAsync(new()
-            {
-                Title = $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}",
-                Screenshots = true,
-                Snapshots = true,
-                Sources = true
-            });
-        }
-
-        [TearDown]
-        public async Task TearDown()
-        {
-            await Context.Tracing.StopAsync(new()
-            {
-                Path = Path.Combine(
-                TestContext.CurrentContext.WorkDirectory,
-                "playwright-traces",
-                $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}.zip"
-            )
-            });
-        }
-
         private const string universalDateFormat = "yyyy-MM-dd";
 
         private static string DateToCalendarIdentifier(DateOnly date)
