a9742a1bb414af307392c48c063f78d09fd5c7f1
Sujan Sapkota <sujan.sapkota@student.hogent.be>
2024-11-07 11:52:00 +0100

    changed so that user can't make two reservations for same timeslot

---
+++ Rise.Persistence/Migrations/20241107104735_UniqueCombinationTimeSlotIdUserIdForReservation.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rise.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueCombinationTimeSlotIdUserIdForReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reservation_UserId",
+                table: "Reservation");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Unique_User_TimeSlot",
+                table: "Reservation",
+                columns: new[] { "UserId", "TimeSlotId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Unique_User_TimeSlot",
+                table: "Reservation");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservation_UserId",
+                table: "Reservation",
+                column: "UserId");
+        }
+    }
+}
