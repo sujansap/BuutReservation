071ce97af703d050bc6d2dae77f8c5b89d142f84
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-11-12 18:28:50 +0100

    revert: :coffin: Removed switching calendar range on index reservation page

--- Rise.Client/Reservations/Index.razor.cs
+++ Rise.Client/Reservations/Index.razor.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components;
 using MudBlazor;
 using Rise.Shared.TimeSlots;
 
-
 namespace Rise.Client.Reservations
 {
     public partial class Index : ComponentBase
@@ -27,61 +26,7 @@ namespace Rise.Client.Reservations
             }
         }
 
-        [Inject]
-        private ITimeSlotService TimeSlotService { get; set; } = default!;
-
-        /// <summary>
-        /// All unavailable days on the calendar
-        /// </summary>
-        private Dictionary<DateOnly, TimeSlotDaySurfaceInfoDto> AvailableDays { get; set; } = [];
-        private List<ColoredCalendarItem> ReservationsOfCurrentUser = new List<ColoredCalendarItem>();
-
-        private DateOnly? SelectedDate { get; set; }
-
-        /// <summary>
-        /// Handle when calendar date range changes
-        /// </summary>
-        /// <param name="dateRange">The new date range</param>
-        private async Task OnDateRangeChanged(DateRange dateRange)
-        {
-            if (dateRange.Start.HasValue && dateRange.End.HasValue)
-            {
-                DateOnly startDate = DateOnly.FromDateTime(dateRange.Start.Value);
-                DateOnly endDate = DateOnly.FromDateTime(dateRange.End.Value);
-                await UpdateDates(startDate, endDate);
-            }
-        }
-
-        /// <summary>
-        /// Update known dates via the api
-        /// </summary>
-        /// <returns></returns>
-        private async Task UpdateDates(DateOnly startDate, DateOnly endDate)
-        {
-
-            try
-            {
-                TimeSlotRangeInfoDto response = await TimeSlotService.GetAllTimeSlotsInRange(
-                startDate,
-                endDate
-            );
-
-
-                AvailableDays = response.Days
-                .Where(day => day.IsSlotAvailable)
-                .ToDictionary(day => day.Date, day => day);
-
-                ReservationsOfCurrentUser = response.Days.Where(day => day.IsBookedByUser).Select(ConvertToCalendarItems).ToList();
-            }
-
-            catch
-            {
-                var errorMessage = Localizer["ErrorLoadingReservations"];
-                Snackbar.Add(new MarkupString($"<span data-testid='error-message'>{errorMessage}</span>"), Severity.Error);
-                return;
-            }
-        }
-
+        [SupplyParameterFromQuery]
         /// <summary>
         /// Which tab is open on the page
         /// </summary>
@@ -114,22 +59,6 @@ namespace Rise.Client.Reservations
             {
                 UpdateSelectedTabInQuery(TabIndex);
             }
-        }
-
-        /// <summary>
-        /// Converts calendar item to highlight on calendar using day info
-        /// </summary>
-        /// <param name="day">day info about the events</param>
-        /// <returns>calendar item</returns>
-        private static ColoredCalendarItem ConvertToCalendarItems(TimeSlotDaySurfaceInfoDto day)
-        {
-            return new ColoredCalendarItem()
-            {
-                Start = day.Date.ToDateTime(TimeOnly.MinValue),
-                End = day.Date.ToDateTime(TimeOnly.MaxValue),
-                Text = "",
-                Color = Color.Primary,
-            };
         }
 
     }
