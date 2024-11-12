9db9785c186f110baa383605514552a354d94c6f
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-11-12 20:13:03 +0100

    refactor: :coffin: Removed commented boat specification

--- Rise.Domain/Boats/IBoat.cs
+++ Rise.Domain/Boats/IBoat.cs
@@ -15,44 +15,6 @@ namespace Rise.Domain.Boats
             set;
         }
 
-
-
-        // ! out of order admin user story 
-        // public DateTime? StartOutOfOrder
-        // {
-        //     get;
-        // }
-
-        // public DateTime? EndOutOfOrder
-        // {
-        //     get;
-        // }
-
-        // ! making reservation user story 
-        // public int MaximumAdults
-        // {
-        //     get;
-        //     set;
-        // }
-
-        // public int MaximumChildren
-        // {
-        //     get;
-        //     set;
-        // }
-
-        // public int MaximumPets
-        // {
-        //     get;
-        //     set;
-        // }
-
-        // ! making reservation user story
-        // public ICollection<IBattery> Batteries
-        // {
-        //     get;
-        // }
-
         public ICollection<IReservation> Reservations
         {
             get;
@@ -62,22 +24,5 @@ namespace Rise.Domain.Boats
         {
             get;
         }
-
-
-        // ! out of order admin user story 
-        // /// <summary>
-        // /// Defines the period for how long the boat is out of order.
-        // /// </summary>
-        // public void DefineOutOfOrderPeriod(DateTime? startOutOfOrder, DateTime? endOutOfOrder = null);
-
-        // /// <summary>
-        // /// Removes out of orders Period
-        // /// </summary>
-        // public void RemoveOutOfOrderPeriod();
-
-        // /// <summary>
-        // /// Checks given date is in the out of order range
-        // /// </summary>
-        // public bool IsInOutOfOrderPeriod(DateTime date);
     }
 }
