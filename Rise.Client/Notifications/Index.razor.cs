8b19b5281ae4ace70d46e4c19d829c873191a520
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-11-18 20:38:12 +0100

    feat: :sparkles: Created a Notifications page

---
+++ Rise.Client/Notifications/Index.razor.cs
@@ -0,0 +1,57 @@
+using System;
+using MudBlazor;
+using Rise.Shared.Notifications;
+
+namespace Rise.Client.Notifications
+{
+    public partial class Index
+    {
+        private NotificationDto[] Notifications { get; set; } = new NotificationDto[0];
+
+        protected override void OnInitialized()
+        {
+            FetchNotifications();
+        }
+        private void FetchNotifications()
+        {
+            Notifications = [
+                new NotificationDto
+                {
+                    Severity = Severity.Success.ToString(),
+                    Title = "Success",
+                    Message = "This is a success message",
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    IsRead = false,
+                },
+                new NotificationDto
+                {
+                    Severity = Severity.Info.ToString(),
+                    Title = "Info",
+                    Message = "This is an info message",
+                    CreatedAt = DateTime.Now.Subtract(TimeSpan.FromMinutes(5)),
+                    UpdatedAt = DateTime.Now.Subtract(TimeSpan.FromMinutes(5)),
+                    IsRead = false,
+                },
+                new NotificationDto
+                {
+                    Severity = Severity.Warning.ToString(),
+                    Title = "Warning",
+                    Message = "This is a warning message",
+                    CreatedAt = DateTime.Now.Subtract(TimeSpan.FromMinutes(10)),
+                    UpdatedAt = DateTime.Now.Subtract(TimeSpan.FromMinutes(10)),
+                    IsRead = false,
+                },
+                new NotificationDto
+                {
+                    Severity = Severity.Error.ToString(),
+                    Title = "Error",
+                    Message = "This is an error message",
+                    CreatedAt = DateTime.Now.Subtract(TimeSpan.FromDays(1).Add(TimeSpan.FromMinutes(5))),
+                    UpdatedAt = DateTime.Now.Subtract(TimeSpan.FromDays(1).Add(TimeSpan.FromMinutes(4))),
+                    IsRead = true,
+                }
+            ];
+        }
+    }
+}
