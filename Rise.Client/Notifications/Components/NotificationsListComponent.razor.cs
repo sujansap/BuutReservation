087e8ca2218d36a2b9cd49462f659bb235eee18a
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-11-18 20:37:59 +0100

    feat: :sparkles: Created a notification list component

---
+++ Rise.Client/Notifications/Components/NotificationsListComponent.razor.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Rise.Shared.Notifications;
+
+namespace Rise.Client.Notifications.Components
+{
+    public partial class NotificationsListComponent
+    {
+        [Parameter, EditorRequired]
+        public required NotificationDto[] Notifications { get; set; }
+
+
+        private Severity GetSeverityFromString(string severity)
+        {
+            return severity switch
+            {
+                "Error" => Severity.Error,
+                "Info" => Severity.Info,
+                "Success" => Severity.Success,
+                "Warning" => Severity.Warning,
+                _ => Severity.Info
+            };
+        }
+    }
+}
