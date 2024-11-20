b60aba89837d6ad6cac5ea2a9b605664278c7450
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-11-20 12:14:12 +0100

    🏗️ feat: add notification and user domain entities

--- Rise.Domain/Users/User.cs
+++ Rise.Domain/Users/User.cs
@@ -1,5 +1,6 @@
 // using Rise.Domain.Reservations;
 
+using Rise.Domain.Notifications;
 using Rise.Domain.Reservations;
 
 namespace Rise.Domain.Users
@@ -18,5 +19,6 @@ namespace Rise.Domain.Users
         }
 
         public ICollection<Reservation> Reservations { get; } = [];
+        public ICollection<Notification> Notifications { get; } = [];
     }
 }
