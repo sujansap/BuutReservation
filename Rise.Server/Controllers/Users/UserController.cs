bf9ef376d5a9bf3eb394f5d16bbe8c0b29b838b0
Sujan Sapkota <sujan.sapkota@student.hogent.be>
2024-11-20 17:02:10 +0100

    feat: api call to get all the users

--- Rise.Server/Controllers/Users/UserController.cs
+++ Rise.Server/Controllers/Users/UserController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Rise.Shared.Users;
 
 namespace Rise.Server.Controllers.Users
 {
@@ -7,6 +8,29 @@ namespace Rise.Server.Controllers.Users
     [ApiController]
     public class UserController : ControllerBase
     {
-        /*TODO:add another features*/
+        private readonly ILogger<ReservationController> _logger;
+        private readonly IUserService _userService;
+
+        public UserController(ILogger<ReservationController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+
+
+        /// <summary>
+        /// Get all the guest users
+        /// </summary>
+        /// <returns>List of the guest users</returns>
+        [HttpGet("guests")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<UserDto>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetGuestUsers()
+        {
+            _logger.LogInformation("GET api/user");
+            var users = await _userService.GetGuestUsers();
+            return Ok(users);
+        }
     }
 }
--- Rise.Services/Users/UserService.cs
+++ Rise.Services/Users/UserService.cs
@@ -1,5 +1,5 @@
-using System;
 using Rise.Persistence;
+using Microsoft.EntityFrameworkCore;
 using Rise.Shared.Users;
 
 namespace Rise.Services.Users;
@@ -8,4 +8,14 @@ public class UserService(ApplicationDbContext dbContext) : IUserService
 {
     private readonly ApplicationDbContext _dbContext = dbContext;
 
+    public async Task<IEnumerable<UserDto>> GetGuestUsers()
+    {
+        return await _dbContext.Users
+        .Select(user => new UserDto()
+        {
+            Id = user.Id,
+            FamilyName = user.FamilyName
+        }).ToListAsync();
+
+    }
 }
