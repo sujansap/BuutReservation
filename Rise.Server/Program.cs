aa74a94b99722e871abd0afd66d0db4d67b46bfa
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-11-20 12:15:46 +0100

    🔌 feat: add notification API endpoints

--- Rise.Server/Program.cs
+++ Rise.Server/Program.cs
@@ -11,6 +11,8 @@ using FluentValidation.AspNetCore;
 using Rise.Server.Middleware;
 using Serilog.Events;
 using Serilog;
+using Rise.Shared.Notifications;
+using Rise.Services.Notifications;
 using Rise.Shared.Users;
 using Rise.Services.Users;
 
@@ -48,6 +50,7 @@ try
 
     builder.Services.AddScoped<ITimeSlotService, TimeSlotService>();
     builder.Services.AddScoped<IReservationService, ReservationService>();
+    builder.Services.AddScoped<INotificationService, NotificationService>();
     builder.Services.AddScoped<IUserService, UserService>();
 
     //validation using fluent validation
