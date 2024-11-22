827b80d2744cfbf93438479e7ec2657b407b450f
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-11-22 17:41:22 +0100

    feat: :recycle: Changed ToString("t") to ToShortTimeString() in extension methods

--- Rise.Client/Utils/ExtensionMethods.cs
+++ Rise.Client/Utils/ExtensionMethods.cs
@@ -16,13 +16,13 @@ namespace Rise.Client.Utils
             //If the timestamp is today, return the time
             if (date.Date == DateTime.Today)
             {
-                return date.ToString("t");
+                return date.ToShortTimeString();
             }
 
             //If the timestamp was yesterday, return "Yesterday" + time
             if (date.Date == DateTime.Today.AddDays(-1))
             {
-                return "Gisteren om " + date.ToString("t");
+                return "Gisteren om " + date.ToShortTimeString();
             }
 
             //If the timestamp is within the last 7 days, return the day of the week and the time
