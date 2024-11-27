e1e5cc5104d97e286b221d80b3461dd75e0909f9
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-11-27 14:02:21 +0100

    test: :white_check_mark: Fixed login method in E2E tests

--- Rise.Client.Tests/CustomAuthenticatedPageTest.cs
+++ Rise.Client.Tests/CustomAuthenticatedPageTest.cs
@@ -24,7 +24,7 @@ namespace Rise.Client.Tests
 
             var credentials = role switch
             {
-                UserRole.Admin => Configuration.GetSection("Administrator").Get<Credentials>(),
+                UserRole.Administrator => Configuration.GetSection("Administrator").Get<Credentials>(),
                 UserRole.Guest => Configuration.GetSection("Guest").Get<Credentials>(),
                 UserRole.Member => Configuration.GetSection("Member").Get<Credentials>(),
                 _ => throw new ArgumentOutOfRangeException(role.ToString(), "Unknown role")
