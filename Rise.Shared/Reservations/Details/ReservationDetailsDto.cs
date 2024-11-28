0166eb0b76eb6db5afdc052ff3371bdef6e0d71b
bas <bas.stokmans@student.hogent.be>
2024-11-28 14:34:36 +0100

    refactored dto and localiser

--- Rise.Shared/Reservations/Details/ReservationDetailsDto.cs
+++ Rise.Shared/Reservations/Details/ReservationDetailsDto.cs
@@ -5,12 +5,12 @@ public class ReservationDetailsDto : BaseDto
     public TimeOnly Start { get; set; }
     public TimeOnly End { get; set; }
     public DateOnly Date { get; set; }
-    public int BoatId { get; set; } 
-    public string BoatPersonalName { get; set; } = default!; 
+    public int BoatId { get; set; }
+    public string BoatPersonalName { get; set; } = default!;
 
     public string? MentorName { get; set; }
 
-    public Boolean IsDeleted { get; set; }
+    public bool IsDeleted { get; set; }
 
     public string? BatteryType { get; set; }
 
