9c60508a98e0df389b85c3907f2a21cdff864c92
bindothorpe <91731690+bindothorpe@users.noreply.github.com>
2024-12-06 11:08:30 +0100

    feat: :sparkles: Implement unread notification count feature

--- Rise.Client/Layout/NavMenu.razor.cs
+++ Rise.Client/Layout/NavMenu.razor.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Rise.Client.Notifications;
+using Rise.Shared.Notifications;
 
 namespace Rise.Client.Layout
 {
@@ -7,13 +9,28 @@ namespace Rise.Client.Layout
     {
         private bool _drawerOpen = false;
         private bool _notificationPopoverOpen = false;
+        private int _unreadNotificationCount = 0;
+
 
         [Inject]
         public required NavigationManager Navigation { get; set; }
+        [Inject]
+        public required INotificationService NotificationService { get; set; }
 
         private void ToggleDrawer()
         {
             _drawerOpen = !_drawerOpen;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await UpdateNotificationCount();
+        }
+
+        private async Task UpdateNotificationCount()
+        {
+            _unreadNotificationCount = await NotificationService.GetUnreadNotificationCount();
+            StateHasChanged();
         }
 
         private void HandleNotificationButtonClicked()
