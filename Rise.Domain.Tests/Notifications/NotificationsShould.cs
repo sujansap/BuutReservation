1b8ed6e32c006613511e6a881b5fc006ddf26919
SimonDeRoeve <125955362+SimonDeRoeve@users.noreply.github.com>
2024-12-11 12:27:53 +0100

    fix: :bug: Removed UserId in tests for notifications

--- Rise.Domain.Tests/Notifications/NotificationsShould.cs
+++ Rise.Domain.Tests/Notifications/NotificationsShould.cs
@@ -15,7 +15,6 @@ namespace Rise.Domain.Tests.Notifications
             notification.Title.ShouldBe(NotificationBuilder.ValidTitle);
             notification.Message.ShouldBe(NotificationBuilder.ValidMessage);
             notification.IsRead.ShouldBe(NotificationBuilder.ValidIsRead);
-            notification.UserId.ShouldBe(NotificationBuilder.ValidUserId);
             notification.User.ShouldBe(NotificationBuilder.ValidUser);
         }
 
